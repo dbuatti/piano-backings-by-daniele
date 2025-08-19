817ecb0b1840f00188d953ff78b14746aca723de
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-08-19 18:41:20 +1000

    [dyad] Created a dedicated form page and updated routing - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import RequestForm from "./pages/RequestForm";
+import FormPage from "./pages/FormPage";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/request-form" element={<RequestForm />} />
+          <Route path="/form-page" element={<FormPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
